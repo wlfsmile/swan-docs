b92b3bcb53dc72cbacb274ccb09cba0cf29cb961
wulinfei <wulinfei@baidu.com>
2019-11-25 15:53:50 +0800

    fate: 搜索数据筛选&搜索sug

--- scripts/program-search.js
+++ scripts/program-search.js
@@ -4,7 +4,19 @@
  */
 
 const fs = require('fs');
-const {BOARD_MAP, TAG_MAP} = require('./utils/constant');
+const {BOARD_MAP, TAG_MAP, BOARD_URL_MAP} = require('./utils/constant');
+
+// 获取导航链接，如果导航不存在link，则找其sidebar第一项的链接
+const findLink = navItem => {
+    if (navItem.link) {
+        return navItem.link;
+    }
+    const sidebar = navItem.sidebar;
+    if (sidebar && sidebar[0].link) {
+        return sidebar[0].link;
+    }
+    return findLink(sidebar[0]);
+};
 
 /* global hexo */
 hexo.extend.generator.register('programSearch', function (locals) {
@@ -14,8 +26,53 @@ hexo.extend.generator.register('programSearch', function (locals) {
 
     let posts;
     let pages;
+    let navs = locals.data.nav;
     // 递归生成搜索结果面包屑
     let resBreadCrumbs = [];
+    // 将侧边栏数据加入搜索结果 && 过滤掉不存在侧边栏的文档
+    navs.forEach(function (nav) {
+        nav.nav.forEach(function (nav) {
+            generateNavItem(nav);
+        });
+    });
+
+    function generateNavItem(navItem) {
+        const link = findLink(navItem);
+        navItem.breadCrumbs = [{name: navItem.text, link: '/docs' + link}];
+        resBreadCrumbs.push(navItem);
+        if (navItem.sidebar) {
+            generateNav(navItem.sidebar, [{name: navItem.text, link: '/docs' + link}], 0);
+        }
+    }
+
+    function generateNav(nav, breadCrumbs, index) {
+        index++;
+        nav.forEach(function (na) {
+            na.breadCrumbs = breadCrumbs.slice(0);
+            resBreadCrumbs.push(na);
+            if (na.sidebar) {
+                const link = findLink(na);
+                const temp = breadCrumbs.slice(0, index);
+                temp.push({name: na.text, link: '/docs' + link});
+                generateNav(na.sidebar, temp, index);
+            }
+        });
+    }
+
+    resBreadCrumbs = resBreadCrumbs.map(function (item) {
+        item.breadCrumbs = item.breadCrumbs.filter(function (subitem) {
+            return subitem.name && subitem.link;
+        });
+
+        item.breadCrumbs.push({name: item.text, link: '/docs' + item.link});
+
+        return {
+            breadCrumbs: item.breadCrumbs,
+            title: item.text,
+            url: '/docs' + item.link,
+            name: item.name
+        };
+    });
 
     if (searchfield.trim() !== '') {
         searchfield = searchfield.trim();
@@ -37,35 +94,37 @@ hexo.extend.generator.register('programSearch', function (locals) {
     let res = [];
     let index = 0;
 
+    const navList = {};
+
+    resBreadCrumbs.forEach(function (item) {
+        // const itemUrl = item.url.trim().replace(/^\//g, '').replace(/\/$/g, '');
+        const itemUrl = item.url.trim().replace(/\//g, '');
+        if (itemUrl.indexOf('#') > 0) {
+            return;
+        }
+        navList[itemUrl] = item.title;
+    });
+
+    posts = posts.filter(function (post) {
+        const postPath = config.root + post.path;
+        const postPathUrl = postPath.trim().replace(/\//g, '');
+        return navList[postPathUrl];
+    });
+
     [posts, pages].forEach(function (posts) {
         if (posts) {
             posts.each(function (post) {
                 if (post.draft) {
                     return;
                 }
-
                 const tmpPost = {};
-                if (post.title) {
-                    tmpPost.title = post.title;
-                }
-                if (post.path) {
-                    tmpPost.url = config.root + post.path;
-                }
+                tmpPost.title = post.title || '';
+                tmpPost.url = config.root + post.path || '';
+                tmpPost.tagName = TAG_MAP[post.nav] || '';
+                tmpPost.boardName = BOARD_MAP[post.header] || '';
+                tmpPost.categoryName = post.categoryName || '';
                 if (post._content) {
                     tmpPost.content = post.more ? replaceHtml(post.more) : includeMarkdown(post._content);
-                }
-                if (post.nav) {
-                    tmpPost.tagName = TAG_MAP[post.nav] || '';
-                } else {
-                    tmpPost.tagName = '';
-                }
-                if (post.header) {
-                    tmpPost.boardName = BOARD_MAP[post.header] || '';
-                } else {
-                    tmpPost.boardName = '';
-                }
-                if (post.categoryName) {
-                    tmpPost.categoryName = post.categoryName;
                 }
                 if (post.tags && post.tags.length > 0) {
                     const tags = [];
@@ -85,25 +144,25 @@ hexo.extend.generator.register('programSearch', function (locals) {
                     post.categories.each(setName);
                     tmpPost.categories = categories;
                 }
-                tmpPost.breadCrumbs = [tmpPost.title + ',' + tmpPost.url.slice(5)];
+                // tmpPost.breadCrumbs = [tmpPost.title + ',' + tmpPost.url.slice(5)];
+                const tmpPostUrl = tmpPost.url.replace(/\//g, '');
+                const tmpPostBreadCrumbs = resBreadCrumbs.filter(function (item) {
+                    const itemUrl = item.url.replace(/\//g, '');
+                    return (itemUrl === tmpPostUrl) && (item.name === post.sidebar);
+                });
+                if (tmpPostBreadCrumbs.length > 0) {
+                    tmpPost.breadCrumbs = tmpPostBreadCrumbs[0].breadCrumbs;
+                }
+                else {
+                    tmpPost.breadCrumbs = [{name: tmpPost.title, link: tmpPost.url}];
+                }
+                tmpPost.breadCrumbs.unshift({name: BOARD_MAP[post.header], link: BOARD_URL_MAP[post.header]});
                 res[index] = tmpPost;
                 index += 1;
             });
         }
     });
-    resBreadCrumbs = resBreadCrumbs.map(function (item) {
-        item.breadCrumbs = item.breadCrumbs.filter(function (item) {
-            return item !== 'undefined,undefined';
-        });
-        item.breadCrumbs.push(item.text + ',' + item.link);
-        return {
-            breadCrumbs: item.breadCrumbs,
-            title: item.text,
-            url: '/docs' + item.link,
-            isNav: true
-        };
-    });
-    res = [...resBreadCrumbs, ...res];
+
     // 删除小游戏文档
     res = res.filter(function (item) {
         return item.url.indexOf('/docs/game/') === -1;
--- themes/docs/source/js/search.js
+++ themes/docs/source/js/search.js
@@ -1,47 +1,67 @@
+/**
+ * @file 搜索框事件处理
+ */
+
+// 节流
+var timer;
+var debounce = function (fn, delay) {
+    return function () {
+        var ctx = this;
+        var args = arguments;
+        clearTimeout(timer);
+        timer = setTimeout(function () {
+            fn.apply(ctx, args);
+        }, (delay ? delay : 300));
+    };
+};
+
+// 渲染搜索sug
+var renderSearchSug = function (keywords, resData, docsIsEmpty, fourmIsEmpty) {
+    var str = '';
+    docsIsEmpty = docsIsEmpty || false;
+    fourmIsEmpty = fourmIsEmpty || true;
+    if (!docsIsEmpty) {
+        resData.forEach(function (list) {
+            str += '<div class="top-search-sug-item">'
+                + '<div class="sug-item-broad-name">'+ list.boardInfo.name + '</div>'
+                + '<ul>';
+            list.forEach(function (item) {
+                var path = `https://smartprogram.baidu.com${item.path}`;
+                str += '<li>'
+                    + '<a href="'+ path +' target="_blank"">'
+                    + ' <span class="sug-item-tag">'+ item.tagName +'</span>'
+                    + '<span class="sug-item-content">'
+                    +     '<p class="sug-item-content-title">'+ item.title +'</p>'
+                    +     '<p class="sug-item-content-des">'+ item.description +'</p>'
+                    + '</span>'
+                    + '</a>'
+                    + '</li>';
+            });
+            str += '</ul>'
+                + '</div>'
+                + '</div>';
+        });
+        str += ' <div class="top-search-sug-more">'
+            +  ` <a href="https://smartprogram.baidu.com/forum/search?word=${keywords}&scope=devdocs" target="_blank">`
+            +  '查看更多 >'
+            +  '</a>'
+            +  '</div>'
+    }
+    else if (docsIsEmpty && !fourmIsEmpty) {
+        str += '<div class="top-search-sug-docs-empty">'
+            +  `<a href="https://smartprogram.baidu.com/forum/search?word=${keywords}&scope=devfourm">`
+            +  '文档没有相关内容，查看社区搜索结果 >'
+            +  '</a>'
+            +  '</div>'
+    }
+    $('#top-search-sug').innerHTML = str;
+};
+
 function searchFunc(searchId, contentId) {
-    var $resultContent = document.getElementById(contentId);
     var $input = document.getElementById(searchId);
     if (!$input || !$('#local-search-input').length) {
         return;
     }
-
-    function toggleArticleContent(isShown) {
-        var isToggle = typeof isShown === 'undefined';
-        var resultStatus, articleStatus;
-        if (isToggle) {
-            var currentStatus = $('#article-main-content').css('display');
-            resultStatus = currentStatus;
-            articleStatus = currentStatus === 'block' ? 'none' : 'block';
-        } else {
-            articleStatus = isShown ? 'block' : 'none';
-            resultStatus = isShown ? 'none' : 'block';
-        }
-        $('#article-main-content').css({
-            display: articleStatus
-        });
-        $($resultContent).css({
-            display: resultStatus
-        });
-    }
-    $('#top-search-box').on('click', function (e) {
-        if ($(e.target).hasClass('reset-search-btn')) {
-            toggleArticleContent(true);
-            $('#top-search-box').removeClass('top-search-box-focus');
-            return;
-        }
-        $('.m-doc-level1').css({
-            'display': 'none'
-        });
-        $(this).addClass('top-search-box-focus');
-        $input.focus();
-    });
-
-    $($input).on('blur', function (e) {
-        $('#top-search-box').removeClass('top-search-box-focus');
-        $('.m-doc-level1').css({
-            'display': 'block'
-        });
-    });
 
     var flag = true;
     $input.addEventListener('compositionstart', function () {
@@ -51,6 +71,46 @@ function searchFunc(searchId, contentId) {
     $input.addEventListener('compositionend', function () {
         flag = true;
     });
+
+    $input.addEventListener('input', function (e) {
+        var $this = this;
+        debounce(function () {
+            if (!flag) {
+                return;
+            }
+            var keywords = $this.value.trim();
+            console.log(222333, keywords)
+            if (keywords.length < 0) {
+                return;
+            } else {
+                $.ajax({
+                    url: '/forum/api/search_main?word=' + keywords + '&scope=devdocs',
+                    dataType: 'json',
+                    type: 'GET',
+                    success: function (res) {
+                        var resData = res.data;
+                        if (resData.length === 0) {
+                            $.ajax({
+                                url: '/forum/api/search_main?word=' + keywords + '&scope=devform',
+                                dataType: 'json',
+                                type: 'GET',
+                                success: function(res) {
+                                    if (res.data.length === 0) {
+                                        renderSearchSug(keywords, [], true, true);
+                                    } else {
+                                        renderSearchSug(keywords, ['文档没有内容，去社区查看>>'], true, false);
+                                    }
+                                }
+                            })
+                        } else {
+                            renderSearchSug(keywords, resData, true);
+                        }
+                    }
+                });
+            }
+        }, 500)();
+    });
+
     //阻止回车刷新页面
     $input.addEventListener('keydown', function (e) {
         var e = e || event;
